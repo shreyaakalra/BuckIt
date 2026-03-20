5d35e65ee2fee1e76a973174dbf0c474feb89c60
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-03-20 23:21:56 +0530

    routing

---
+++ src/app/(home)/navbar.tsx
@@ -0,0 +1,7 @@
+export const Navbar = () => {
+    return(
+        <nav className="h-20 flex border-b justify-betweenfont-medium bg-white">
+
+        </nav>
+    )
+}
--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,7 +6,7 @@ import { Textarea } from "@/components/ui/textarea";
 
 export default function Home() {
   return (
-   <div className="flex flex-col gap-y-4 p-4">
+   <div className="flex flex-col gap-y-4 p-4 w-dvw h-dvh">
     <div>
       <Button variant="elevated">
         I am a button
