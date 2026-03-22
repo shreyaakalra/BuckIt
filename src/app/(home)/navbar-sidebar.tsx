a16598f15a66679a60616e09549bd009662a3b92
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-03-22 16:58:24 +0530

    navbar-sidebar attributes

--- src/app/(home)/navbar-sidebar.tsx
+++ src/app/(home)/navbar-sidebar.tsx
@@ -1,4 +1,6 @@
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { Sheet, SheetContent, SheetHeader, SheetTitle } from "@/components/ui/sheet";
+import Link from "next/link";
 
 interface NavbarItem {
     href: string;
@@ -29,6 +31,18 @@ export const NavbarSidebar = ({
                         </SheetTitle>
                     </div>
                 </SheetHeader>
+                <ScrollArea className="flex flex-col overflow-y-auto h-full pb-2">
+                    {items.map((item) => (
+                        <Link
+                            key={item.href}
+                            href={item.href}
+                            className="w-full text-left p-4 hover:bg-black hover:text-white flex items-center text-base font-medium"
+                        >
+                            {item.children}
+                        </Link>
+                    ))}
+
+                </ScrollArea>
 
             </SheetContent>
         </Sheet>
