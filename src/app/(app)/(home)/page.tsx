902691b17786322e4ab24a5151019b0b67d8b9ac
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-03-23 14:49:51 +0530

    payload integration

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,29 +1,11 @@
-import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Input } from "@/components/ui/input";
-import { Progress } from "@/components/ui/progress";
-import { Textarea } from "@/components/ui/textarea";
+import configPromise from "@payload-config"
+import { getPayload } from 'payload'
 
-export default function Home() {
+export default async function Home() {
+  
   return (
-   <div className="flex flex-col gap-y-4 p-4 w-dvw h-dvh">
-    <div>
-      <Button variant="elevated">
-        I am a button
-      </Button>
-    </div>
-    <div>
-      <Input placeholder="I am an input" />
-    </div>
-    <div>
-      <Progress value={50} />
-    </div>
-    <div>
-      <Textarea placeholder="I am a textarea" />
-    </div>
-    <div>
-      <Checkbox />
-    </div>
+   <div>
+    
    </div>
   );
 }
