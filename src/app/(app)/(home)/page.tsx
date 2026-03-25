c28e3dc4b976c0cc48829b42e773c99f0f5eab67
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-03-25 23:57:36 +0530

    auth

--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,8 +1,15 @@
 "use client"
 
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+
 export default function Home() {
+
+  const trpc = useTRPC();
+  const {data} = useQuery(trpc.auth.session.queryOptions());
   
   return (
+    
    <div>
    Home
    </div>
--- src/modules/auth/server/procedures.tsx
+++ src/modules/auth/server/procedures.tsx
@@ -1,12 +1,82 @@
 import { baseProcedure, createTRPCRouter } from "@/trpc/init";
-import { headers as getHeaders } from "next/headers";
+import { TRPCError } from "@trpc/server";
+import { headers as getHeaders, cookies as getCookies} from "next/headers";
+import z from "zod";
 
 export const authRouter = createTRPCRouter({
-    session: baseProcedure.query(async ({ctx}) => {
-        const headers = await getHeaders();
+  session: baseProcedure.query(async ({ ctx }) => {
+    const headers = await getHeaders();
 
-        const session = await ctx.payload.auth({headers});
+    const session = await ctx.payload.auth({ headers });
 
-        return session;
+    return session;
+  }),
+
+  register: baseProcedure
+    .input(
+      z.object({
+        email: z.string().email(),
+        password: z.string(),
+        username: z
+          .string()
+          .min(3, "Username must be at least 3 characters")
+          .max(63, "Username must be less than 63 characters")
+          .regex(
+            /^[a-z0-9][a-z0-9-]*[a-z0-9]$/,
+            "Username can only contain lowercase letters, numbers and hyphens. It must star and end with a letter or number",
+          )
+          .refine(
+            (val) => !val.includes("--"),
+            "Username cannot cotain consecutive hyphens",
+          )
+          .transform((val) => val.toLowerCase()),
+      }),
+    )
+    .mutation(async ({ input, ctx }) => {
+      await ctx.payload.create({
+        collection: "users",
+        data: {
+          email: input.email,
+          username: input.username,
+          password: input.password,
+        },
+      });
     }),
-})
+
+
+login: baseProcedure
+  .input(
+    z.object({
+      email: z.string().email(),
+      password: z.string(),
+    }),
+  )
+  .mutation(async ({ input, ctx }) => {
+    const data = await ctx.payload.login({
+      collection: "users",
+      data: {
+        email: input.email,
+        password: input.password,
+      },
+
+
+    });
+
+    if(!data.token){
+        throw new TRPCError({
+            code: "UNAUTHORIZED",
+            message: "Failed to login"
+        })
+    }
+
+    const cookies = await getCookies();
+    cookies.set({
+        name: "AUTH_COOKIE",
+        calue: data.token,
+        httpOnly: true,
+        path: "/",
+    });
+
+  }),
+
+});
