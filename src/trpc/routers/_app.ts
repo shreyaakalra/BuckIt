c369fd00dd1f49282495e72cf9fa95915089837c
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-03-27 21:04:54 +0530

    checkout page

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -4,11 +4,13 @@ import { createTRPCRouter } from '../init';
 import { categoriesRouter } from '@/modules/categories/server/procedures';
 import { productsRouter } from '@/modules/products/server/procedures';
 import { tagsRouter } from '@/modules/tags/server/procedures';
+import { checkoutRouter } from '@/modules/checkout/server/procedures';
  
 export const appRouter = createTRPCRouter({
   auth: authRouter,
   tags: tagsRouter,
   tenants: tenantsRouter,
+  checkout: checkoutRouter,
   products: productsRouter,
   categories: categoriesRouter
 });
