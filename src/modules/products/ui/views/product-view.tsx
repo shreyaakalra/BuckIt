c1d622e3f1de16676ee217684cae541603500c2f
Shreya Kalra <Shreyaa.kalra@gmail.com>
2026-03-27 20:51:43 +0530

    cart and checkout

--- src/modules/products/ui/views/product-view.tsx
+++ src/modules/products/ui/views/product-view.tsx
@@ -7,12 +7,22 @@ import Image from "next/image";
 import { Fragment } from "react";
 import { LinkIcon, StarIcon } from "lucide-react";
 import { useSuspenseQuery } from "@tanstack/react-query";
-
+import dynamic from "next/dynamic";
 import { useTRPC } from "@/trpc/client";
 import { Button } from "@/components/ui/button";
 import { Progress } from "@/components/ui/progress";
 import { StarRating } from "@/components/star-rating";
 import { formatCurrency, generateTenantURL } from "@/lib/utils";
+
+const CartButton = dynamic(
+  () => import("../components/cart-button").then(
+    (mod) => mod.CartButton,
+  ),
+  {
+    ssr: false,
+    loading: () => <Button disabled className="flex-1 bg-pink-400">Add to cart</Button>
+  },
+);
 
 interface ProductViewProps {
   productId: string;
@@ -100,12 +110,10 @@ export const ProductView = ({ productId, tenantSlug }: ProductViewProps) => {
             <div className="border-t lg:border-t-0 lg:border-l h-full">
               <div className="flex flex-col gap-4 p-6 border-b">
                 <div className="flex flex-row items-center gap-2">
-                  <Button
-                    variant="elevated"
-                    className="flex-1 bg-pink-400"
-                  >
-                    Add to cart
-                  </Button>
+                  <CartButton
+                    productId={productId}
+                    tenantSlug={tenantSlug}
+                  />
                   <Button
                     className="size-12"
                     variant="elevated"
